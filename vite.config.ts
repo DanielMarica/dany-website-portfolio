c1c6ccdff92d046f48ce1663e4eaf1a7ccd053f3
livius.marica <livius.marica@student.vinci.be>
2025-06-24 11:34:43 +0200

    fix : vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  base : "/dany-website-portfolio",
-});
+  base: mode === "production" ? "/dany-website-portfolio/" : "/",
+}));
